ac53de9c74d1a2aae1934bf15438427fdf3eda45
maggivega <31355214+maggivega@users.noreply.github.com>
2017-11-10 14:00:33 -0600

    Add files via upload

---
+++ pregunta2.cc
@@ -0,0 +1,13 @@
+//el progama imprime el valor de x
+#include <iostream>
+using namespace std;
+
+int main(){
+  for (int x=1; x<=5; x=x+2){
+    if (x%3==0){}
+    else {
+      cout<<"x= "<<x<<endl;
+    }
+  }
+  return 0;
+}
